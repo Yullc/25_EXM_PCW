ba994b0ea39107a81659444893dce2a8bef45b09
Yullc <apanan0910@gmail.com>
2025-03-20 15:46:30 +0900

    2. n부터 5까지

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,15 +5,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        for (int i = 0; i < b; i++) {
+       Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+       for (int i = n; i >=1; i--) {
+           System.out.print(i);
+           for (int j = i-1; j >= 1; j--) {
+               System.out.print(j);
+           }
+           System.out.println();
+           n--;
+       }
+    }
+}
 
-            for (int j = 0; j < a-1; j++) {
-                System.out.print("*");
-            }
-            System.out.println("*");
-        }
-    }
-    }
+//Scanner sc = new Scanner(System.in);
+//int a = sc.nextInt();
+//int b = sc.nextInt();
+//        for (int i = 0; i < b; i++) {
+//        for (int j = 0; j < a-1; j++) {
+//        System.out.print("*");
+//            }
+//                    System.out.println("*");
+//        }
