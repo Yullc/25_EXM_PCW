19ed9e27af90a7b35f5b52787b397cb4f461ab4a
Yullc <apanan0910@gmail.com>
2025-03-20 16:29:58 +0900

    2. 피보나치

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,18 @@
 package org.example;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-       int n = sc.nextInt();
-       for (int i = n; i >=1; i--) {
-           System.out.print(i);
-           for (int j = i-1; j >= 1; j--) {
-               System.out.print(j);
-           }
-           System.out.println();
-           n--;
-       }
+     int[] arr =new int[11];
+
+     for (int i = 0; i <arr.length; i++) {
+         arr[i]=arr[i]+1+arr[i+2];
+         arr[i+2] = arr[i] + arr[i+1];
+         System.out.print(" "+arr[i]);
+        }
     }
 }
 
