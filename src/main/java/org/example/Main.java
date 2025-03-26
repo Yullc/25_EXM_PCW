2404c16b47df1e3b46a238bfbb69e3f3d1952c2d
Yullc <apanan0910@gmail.com>
2025-03-26 16:53:42 +0900

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,4 @@
-package org.example;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+package org.example;
 
 import java.util.Arrays;
 import java.util.Scanner;
@@ -6,22 +6,80 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-     int[] arr =new int[11];
-
-     for (int i = 0; i <arr.length; i++) {
-         arr[i]=arr[i]+1+arr[i+2];
-         arr[i+2] = arr[i] + arr[i+1];
-         System.out.print(" "+arr[i]);
-        }
+       Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+       int m =1;
+       for(int i = n; i > 0; i--) {
+           for(int j =0; j<n-i; j++){
+               System.out.print(" ");
+           }
+           for(int k =n; k-m>=0; k--){
+               System.out.print(k-(m-1)+" ");
+           }
+           System.out.println();
+           m++;
+       }
     }
 }
-
-//Scanner sc = new Scanner(System.in);
-//int a = sc.nextInt();
-//int b = sc.nextInt();
-//        for (int i = 0; i < b; i++) {
-//        for (int j = 0; j < a-1; j++) {
-//        System.out.print("*");
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int m =1;
+//        for(int i = n; i>0; i--){
+//            for(int j = 0; j<n-i; j++){
+//                System.out.print(" ");
 //            }
-//                    System.out.println("*");
+//            for(int k = n; k-m>=0; k--){
+//                System.out.print(k-(m-1)+" ");
+//            }
+//            System.out.println();
+//            m++;
 //        }
+//    }
+//}
+
+
+//    public static void main(String[] args) {
+//    Solution s = new Solution();
+//    int numlist[] = {4, 5, 6, 7, 8, 9, 10, 11, 12};
+//    s.solution(3,numlist);
+//    }
+//}
+//class Solution {
+//    public int[] solution(int n, int[] numlist) {
+//        int[] answer = {};
+//        int a = 0;
+//        for (int i = 0; i < numlist.length; i++) {
+//            if(numlist[i]%n==0){
+//                a = numlist[i];
+//            }
+//            for(int j=0;j<answer.length;j++){
+//                answer[j] = a;
+//            }
+//        }
+//        return answer;
+//    }
+//}
+//
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
